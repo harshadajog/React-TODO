7d4ec3d6a83309bed104f510ae3b29e37299af2d
Netapp DPG <netappdpg@Netapps-Air.lan>
2021-12-14 13:59:16 -0800

    Created todo with basic add, edit and delete tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
+import { useState } from 'react'; 
+import { nanoid } from "nanoid";
 
-function App() {
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState('All');
+  const taskList = tasks.map(task => <Todo id={task.id} key={task.id} name={task.name} completed={task.completed} editTask={editTask} deleteTask={deleteTask} toggleTaskCompleted={toggleTaskCompleted} />);
+  const filterList = FILTER_NAMES.map(name => <FilterButton key={name} name={name} />);
+
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTasks = tasks.map(task => {
+      if(id === task.id){
+        return {...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTasks);
+  }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id){
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks':'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+       {filterList}
+      </div>
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+       {taskList}
+      </ul>
     </div>
   );
 }
